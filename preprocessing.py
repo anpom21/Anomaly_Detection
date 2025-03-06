cfa25710bfb0f2ca0780e9e20a100aba49f50bda
Andreas Pommerencke <anpom21@student.sdu.dk>
2025-03-06 16:03:02 +0100

    preprocessing v2

--- preprocessing.py
+++ preprocessing.py
@@ -7,12 +7,39 @@
 # Reconstructive surface normals
 # Mosaic
 
+# ---------------------------------------------------------------------------- #
+#                                  How to use                                  #
+# ---------------------------------------------------------------------------- #
+"""
+from preprocessing import preprocess_images, preview_layered_images
+import numpy as np
+
+PATH = 'path\to\images\Train' # ie 'Datasets\Dataset001\Train'
+
+images = preprocess_images(PATH)
+preview_layered_images(images)
+
+# Load .npy file
+images = np.load('layered_images.npy')
+preview_layered_images(images)
+
+"""
+# ---------------------------------------------------------------------------- #
+#                                    Import                                    #
+# ---------------------------------------------------------------------------- #
 import os
 import cv2
 import numpy as np
+import torch
+from torchvision import transforms
 
-
+# ---------------------------------------------------------------------------- #
+#                                   Function                                   #
+# ---------------------------------------------------------------------------- #
+# -------------------------------- Load images ------------------------------- #
 # Load images from the dataset
+
+
 def load_images(path):
     image_paths = []
     listdir = os.listdir(path)
@@ -27,16 +54,7 @@ def load_images(path):
         images.append(image)
     return images
 
-# Display images
-
-
-def preview_images(images):
-    for image in images:
-        # Display image
-        cv2.imshow('image', image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
+# ---------------------------------- Resize ---------------------------------- #
 # Reisze images to 256x256
 
 
@@ -51,6 +69,7 @@ def resize_images(images, width=256, height=256):
             resized_images.append(image)
     return resized_images
 
+# --------------------------------- Greyscale -------------------------------- #
 # Convert images list to greyscale
 
 
@@ -61,6 +80,7 @@ def greyscale_images(images):
         greyscale_images.append(greyscale_image)
     return greyscale_images
 
+# ------------------------------- Layer images ------------------------------- #
 # Layer images into groups of n images
 
 
@@ -74,31 +94,59 @@ def layer_images(images, n=4):
         partitioned_images.append(images[i:i+n])
     partitioned_images = np.array(partitioned_images)
 
-    print('Images imported succesfully with shape:')
-    print(partitioned_images.shape)
-
     return partitioned_images
 
-
+# -------------------------- Preview Layered Images -------------------------- #
 # Display layered images with image number and light position indicated
-def preview_layered_images(layered_images):
-    print('Layered images:', layered_images.shape)
-    for layer_i, layer in enumerate(layered_images):
+
+
+def preview_layered_images(layered_images, amount=4):
+    assert type(layered_images) == np.ndarray, 'Input must be a numpy array'
+    print(f'Displaying first {amount} layers.')
+    for layer_i, layer in enumerate(layered_images[:amount]):
         for i, image in enumerate(layer):
             # Display image
-            cv2.imshow(f'Image {layer_i}, Light pos {i}', image)
+            cv2.imshow(f'Image {layer_i}, Light {i}', image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
-
-# Input path to the dataset
-path = 'Datasets\Dataset001\Train'
-
-images = load_images(path)
-images = resize_images(images)
-images = greyscale_images(images)
-images = layer_images(images, 4)
-tensor_images =
+# ----------------------------- Preprocess images ---------------------------- #
 
 
-preview_layered_images(images)
+def preprocess_images(folder_path, n=4, width=256, height=256):
+
+    try:
+        images = load_images(folder_path)
+        images = resize_images(images, width, height)
+        images = greyscale_images(images)
+        images = layer_images(images, n)
+        np.save('layered_images.npy', images)
+        print('Images saved succesfully:', images.shape)
+        return images
+    except Exception as e:
+        print('Error while preprocessing images:', e)
+# ---------------------------------------------------------------------------- #
+#                                     Main                                     #
+# ---------------------------------------------------------------------------- #
+
+
+if __name__ == '__main__':
+
+    # Test preprocessing functions
+    # ------------------------- Input path to the dataset ------------------------ #
+    path = 'Datasets\Dataset001\Train'
+
+    # ------------------------ Run preprocessing functions ----------------------- #
+    images = preprocess_images(path)
+
+    # --------------------------- Save and load images --------------------------- #
+    np.save('layered_images.npy', images[0:4])
+    print('Images saved succesfully', images[0:4].shape)
+
+    image_loaded = np.load('layered_images.npy')
+    print('Loaded image shape:', image_loaded.shape)
+
+    # Preview
+    print('Images saved succesfully')
+
+    preview_layered_images(image_loaded)
