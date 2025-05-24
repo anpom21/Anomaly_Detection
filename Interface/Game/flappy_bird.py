4a15fa5def3e2d6f89828f9875465613b108cd6d
anpom21 <anpom21@student.sdu.dk>
2025-05-24 08:19:37 +0200

    Game Changes

--- Interface/Game/flappy_bird.py
+++ Interface/Game/flappy_bird.py
@@ -5,8 +5,8 @@ import pygame
 import random
 import sys
 import time
-from Game.read_bpm import start_heart_rate_stream, bpm_data
-import Game.read_bpm
+from read_bpm import start_heart_rate_stream, bpm_data
+import read_bpm
 import yaml
 import os
 
@@ -15,11 +15,12 @@ import os
 # ---------------------------------------------------------------------------- #
 clock = pygame.time.Clock()
 working_directory = os.path.dirname(os.path.abspath(__file__))
+scaling = 1.5
 
 
 def wait_for_start_zone(bird_rect, background_img, drone_img, screen, font):
     global clock
-    start_zone = pygame.Rect(40, 40, 120, 30)
+    start_zone = pygame.Rect(40*scaling, 40*scaling, 120*scaling, 30*scaling)
     bird_velocity = 0
 
     while True:
@@ -52,15 +53,19 @@ def wait_for_start_zone(bird_rect, background_img, drone_img, screen, font):
 
 def game_over(bird_rect, score, drone_img, screen, font, background_img, background_x, clock):
     # Define start zone as restart trigger
-    start_zone = pygame.Rect(40, 150, 300, 30)
+    start_zone = pygame.Rect(40*scaling, 150*scaling, 300*scaling, 30*scaling)
     bird_velocity = 0
     # Load gameover image
-    gameover_img = pygame.image.load(os.path.join(working_directory, "game_over.png")).convert_alpha()
-    gameover_img = pygame.transform.scale(gameover_img, (186, 110))
-    gameover_pos = (screen.get_width() // 2 - 100, 10)
+    gameover_img = pygame.image.load(os.path.join(
+        working_directory, "game_over.png")).convert_alpha()
+    gameover_img = pygame.transform.scale(
+        gameover_img, (186*scaling, 110*scaling))
+    # gameover_pos = (screen.get_width() // 2 - 100, 10)
 
-    score_bacground_img = pygame.image.load(os.path.join(working_directory, "score_background.png")).convert_alpha()
-    score_bacground_img = pygame.transform.scale(score_bacground_img, (186, 110))
+    score_bacground_img = pygame.image.load(os.path.join(
+        working_directory, "score_background.png")).convert_alpha()
+    score_bacground_img = pygame.transform.scale(
+        score_bacground_img, (186*scaling, 110*scaling))
     # Shape of the score background
 
     # Load or initialize high score
@@ -80,6 +85,7 @@ def game_over(bird_rect, score, drone_img, screen, font, background_img, backgro
             yaml.dump(high_score, file)
 
     # Game over loop
+    first_run = True
     while True:
         clock.tick(60)
         keys = pygame.key.get_pressed()
@@ -92,38 +98,40 @@ def game_over(bird_rect, score, drone_img, screen, font, background_img, backgro
         # Move bird so it can "fly into" start zone to restart
         bird_velocity = update_bird_velocity(bird_velocity, keys)
         bird_rect.y += bird_velocity
-
+        if bird_rect.y > 900 * scaling:
+            first_run = False
         # Check if player wants to restart
-        if bird_rect.colliderect(start_zone):
+        if bird_rect.colliderect(start_zone) and not first_run:
             return bird_rect, background_x  # Reset to caller with new position
 
         # Draw everything
         screen.blit(background_img, (background_x, 0))
-        screen.blit(background_img, (background_x + 587, 0))
+        screen.blit(background_img, (background_x + 587*scaling, 0))
         screen.blit(drone_img, bird_rect)
 
         pygame.draw.rect(screen, (255, 0, 0), start_zone)
-        font = pygame.font.SysFont(None, 48)
+        font = pygame.font.SysFont(None, int(48*scaling))
         restart_text = font.render(
             "Fly Here to Restart", True, (255, 255, 255))
         screen.blit(restart_text, (start_zone.centerx - restart_text.get_width() // 2,
                                    start_zone.centery - restart_text.get_height() // 2))
 
-        screen.blit(gameover_img, (screen.get_width() // 2-186//2, 10))
+        screen.blit(gameover_img, (screen.get_width() //
+                    2-186*scaling//2, 10*scaling))
         # --- Score display --- #
-        font = pygame.font.SysFont(None, 35)
+        font = pygame.font.SysFont(None, int(35*scaling))
         screen.blit(score_bacground_img,
-                    (screen.get_width() // 2-186//2, 200))
+                    (screen.get_width() // 2-186*scaling//2, 200*scaling))
 
         # Score
         score_text = font.render(f"Score: {score}", True, (255, 255, 255))
         screen.blit(score_text, (screen.get_width() //
-                    2 - score_text.get_width() // 2, 215))
+                    2 - score_text.get_width() // 2, 215*scaling))
         # High score
         high_score_text = font.render(
             f"High Score: {high_score}", True, (255, 255, 255))
         screen.blit(high_score_text, (screen.get_width() //
-                    2 - high_score_text.get_width() // 2, 260))
+                    2 - high_score_text.get_width() // 2, 260*scaling))
 
         pygame.display.update()
 
@@ -134,7 +142,7 @@ def bpm_to_speed(bpm):
     # Min speed 3.0
 
     speed_scaling = 1
-    delta_bpm = Game.read_bpm.initial_bpm - bpm
+    delta_bpm = read_bpm.initial_bpm - bpm  # Changed
     speed = 4.0 + delta_bpm * speed_scaling
 
     # Ensure speed is within bounds
@@ -167,9 +175,9 @@ def update_bird_velocity(velocity, keys, acceleration=0.5, max_speed=6, friction
 def read_position(bird_rect):
     # Read the position from the file
     keys = pygame.key.get_pressed()
-    acceleration = 1
-    max_speed = 6.5
-    friction = 10
+    acceleration = 1*scaling
+    max_speed = 6.5*scaling
+    friction = 10*scaling
     bird_speed = update_bird_velocity(
         bird_speed, keys, acceleration, max_speed, friction)
     bird_position = bird_rect.y + bird_speed
@@ -185,13 +193,14 @@ def run_game():
     pygame.init()
 
     # Screen settings
-    WIDTH, HEIGHT = 400, 600
+    WIDTH, HEIGHT = 400*scaling, 600*scaling
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
 
     # Background setup
-    background_width = 587
-    background_img = pygame.image.load(os.path.join(working_directory, "background_looping.png")).convert()
+    background_width = 587*scaling
+    background_img = pygame.image.load(os.path.join(
+        working_directory, "background_looping.png")).convert()
     background_img = pygame.transform.scale(
         background_img, (background_width, HEIGHT))  # 587x600
     background_start_x = 0
@@ -200,31 +209,36 @@ def run_game():
 
     # Bird setup (pos 0-565)
     bird_speed = 4
-    bird_width = 35
-    bird_height = 35
-    drone_img = pygame.image.load(os.path.join(working_directory, "drone_white.png")).convert_alpha()
+    bird_width = int(35*scaling)
+    bird_height = int(35*scaling)
+    drone_img = pygame.image.load(os.path.join(
+        working_directory, "drone_white.png")).convert_alpha()
     # Scale the image to fit the bird size
     drone_img = pygame.transform.scale(drone_img, (bird_width, bird_height))
-    bird_rect = pygame.Rect(50, HEIGHT // 2, bird_width, bird_height)
+    bird_rect = pygame.Rect(50*scaling, HEIGHT // 2, bird_width, bird_height)
 
     # Pipe setup
-    pipe_width = 60
-    pipe_gap = 60
-    pipe_height = random.randint(150, 450)
+    pipe_width = 60*scaling
+    pipe_gap = 60*scaling
+    pipe_height = random.randint(int(150*scaling), int(450*scaling))
     pipe_x = WIDTH
     pipe_speed = 3
-    laser_img = pygame.image.load(os.path.join(working_directory, "laser4.png")).convert_alpha()
+    laser_img = pygame.image.load(os.path.join(
+        working_directory, "laser4.png")).convert_alpha()
     laser_width, laser_height = laser_img.get_size()
+    pipe_scaling = 1.2 * pipe_width / laser_width
+    laser_img = pygame.transform.scale(
+        laser_img, (laser_width*pipe_scaling, laser_height))
     top_pipe = pygame.Rect(pipe_x, 0, pipe_width, pipe_height)
     bottom_pipe = pygame.Rect(
         pipe_x, pipe_height + pipe_gap, pipe_width, HEIGHT - pipe_height - pipe_gap)
 
     # Score
-    font = pygame.font.SysFont(None, 48)
+    font = pygame.font.SysFont(None, int(48*scaling))
     score = 0
 
     # Heart rate
-    font = pygame.font.SysFont(None, 48)
+    font = pygame.font.SysFont(None, int(48*scaling))
     heart_rate = 60  # Example heart rate
 
     # Game loop
@@ -266,7 +280,7 @@ def run_game():
             pipe_x = WIDTH-pipe_width
 
         # Adjust pipe speed based on heart rate
-        pipe_x -= bpm_to_speed(Game.read_bpm.bpm_data)
+        pipe_x -= bpm_to_speed(read_bpm.bpm_data)  # Changed
         if pipe_x < -pipe_width:
             pipe_x = WIDTH
             pipe_height = random.randint(150, 450)
@@ -314,9 +328,10 @@ def run_game():
         # ------------------------------- Score display ------------------------------ #
         score_text = font.render(str(score), True, (255, 255, 255))
         heart_rate_text = font.render(
-            str(Game.read_bpm.bpm_data), True, (255, 0, 0))
+            str(read_bpm.bpm_data), True, (255, 0, 0))  # Changed
         screen.blit(heart_rate_text, (10, 10))
-        screen.blit(score_text, (WIDTH // 2 - score_text.get_width() // 2, 20))
+        screen.blit(score_text, (WIDTH // 2 -
+                    score_text.get_width() // 2, 20*scaling))
 
         if start_timer == 0:
             start_timer = pygame.time.get_ticks()
